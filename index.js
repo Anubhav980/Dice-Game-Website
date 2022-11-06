358a0fbe759a4cd19e678fca02b0be0351ea20bf
Anubhav Raj <76939615+Anubhav980@users.noreply.github.com>
2022-11-06 08:41:04 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+
+var die = Math.floor(Math.random()*6)+1;
+
+var ab=  "images/dice" + die + ".png";
+
+document.querySelectorAll("img")[0].setAttribute("src",ab);
+
+var de = Math.floor(Math.random()*6)+1;
+
+var cd=  "images/dice" + de + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src",cd);
+
+if(die>de)
+{
+  document.querySelector("h1").innerHTML = "🚩 Player 1 Wins";
+}
+else if (de > die)
+{
+  document.querySelector("h1").innerHTML = "Player 2  Wins 🚩";
+}
+else if (de === die)
+{
+  document.querySelector("h1").innerHTML = "Draw!";
+}
